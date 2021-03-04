babf69862073f72c39e5ca0346882c01e28500f0
Renato Richard <renatorichard2012@gmail.com>
2021-03-04 01:34:55 -0300

    Conclusao modulo mapeamentos basicos

--- src/main/java/com/algaworks/ecommerce/model/Cliente.java
+++ src/main/java/com/algaworks/ecommerce/model/Cliente.java
@@ -5,15 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
-import java.util.Objects;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name="produto")
-public class Produto {
+@Table(name="cliente")
+public class Cliente {
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +19,7 @@ public class Produto {
 
     private String nome;
 
-    private String descricao;
-
-    private BigDecimal preco;
+    @Column(name="sexo_cliente")
+    @Enumerated(EnumType.STRING)
+    private SexoCliente sexo;
 }
