acc1532df1643179bd42a639c1414f8e9abbd80b
Renato Richard <renatorichard2012@gmail.com>
2021-03-08 23:05:01 -0300

    corrige

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @Entity
 @Table(name="produto")
 public class Produto {
+    
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
